ce289c1b894986497183a25049fddcdf13b9e2fe
Lívia Gouvêa <livia@mail.mayfirst.org>
2024-11-01 15:10:44 -0300

    Inicia plugin

---
+++ fedipress.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Fedipress
+ * Description: A plugin to customize Frieds Ppugin layout
+ * Version: 1.0
+ * Author: Lívia Gouvêa
+ */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+
+
