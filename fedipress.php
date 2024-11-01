bc0768d84338be059077433dc868fb6ddd8b0361
Lívia Gouvêa <livia@mail.mayfirst.org>
2024-11-01 15:11:40 -0300

    Sobrescreve CSS

--- fedipress.php
+++ fedipress.php
@@ -11,5 +11,8 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-
+function fedipress_enqueue_scripts() {
+    wp_enqueue_style('meu-plugin-estilo', plugin_dir_url(__FILE__) . 'css/friends.css');
+}
+add_action('wp_enqueue_scripts', 'fedipress_enqueue_scripts');
 
